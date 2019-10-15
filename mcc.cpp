95b449e6958c77c0b835ed91e86b1651a05ace27
Sebastian Werhausen <swerhausen@gmail.com>
2019-10-15 19:12:55 +0200

    added rudimentary memory corruption function for testing

--- mcc.cpp
+++ mcc.cpp
@@ -93,6 +93,13 @@ std::optional<int> get_memory_reserve_from_args(int argc, char* argv[]) {
 }
 
 
+void corrupt_memory(std::vector<OneGB>& memory) {
+	if (rand() >100)
+		return;
+	(*(memory[3].m_data))[5] = 1;
+}
+
+
 int main(int argc, char* argv[]) {
 	constexpr int default_memory_reserve = 2;
 	const int gb_reserve = get_memory_reserve_from_args(argc, argv).value_or(default_memory_reserve);
